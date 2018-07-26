fa0ac03d2ae8b61002be6b921e78eaae81bc4a3b
Osterman <josterma@UR.Rochester.edu>
2018-07-26 12:10:45 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var maintenanceMode = false;
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
+
+app.use((req,res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append to server.log');
+    }
+  });
+  next();
+})
+
+app.use((req,res,next) => {
+  if (maintenanceMode) {
+    res.render('maintenance.hbs');
+  } else {
+    next();
+  }
+})
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+})
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home',
+    welcomeMessage: 'Welcome to localhost three thousand'
+  });
+});
+
+app.get('/about', (req,res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About'
+  });
+});
+
+app.get('/bad', (req,res) => {
+  res.send({
+    errorMessage: "A problem occurred",
+    errorCode: "000123"
+  })
+});
+
+app.listen(3000);
